ef4457c9ddb7844c099422a9771856fc8e7a7203
Tayse Rosa <tayse_rosa@yahoo.com.br>
2023-11-27 16:10:16 -0300

    Add script musics working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.body.addEventListener('keydown', (event)=>{
-    playSound(event.code.toLowerCase)
+document.body.addEventListener('keyup', (event)=>{
+    playSound( event.code.toLowerCase() );
 });
 
 function playSound(sound){
-    let audioElement = document.querySelector(`$s_${sound}`);
+    let audioElement = document.querySelector(`#s_${sound}`);
 
     if(audioElement){
         audioElement.play();
